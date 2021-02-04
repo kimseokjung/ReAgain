eba466073d8ffb81162fa9b0721f47caeda6a1e9
kimseokjung <slsms311@gmail.com>
2021-02-04 10:32:12 +0900

    android commit 21/02/04

---
+++ app/src/main/java/com/example/reagain/ShopFragment.java
@@ -0,0 +1,32 @@
+package com.example.reagain;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class ShopFragment extends Fragment {
+    TextView tv;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_shop, container, false);
+
+        tv = v.findViewById(R.id.shop_tv);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), MapsActivity.class));
+            }
+        });
+
+        return v;
+    }
+}
